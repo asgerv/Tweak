ea3c5b5045b3fc06f4b7e8c4eb37a6519a012ee2
Asger Vestbjerg <asgerv@gmail.com>
2016-06-13 05:01:57 +0200

    Ændringer i CMS Views

--- MVCForum.Website/Controllers/CMSController.cs
+++ MVCForum.Website/Controllers/CMSController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCForum.Domain.Constants;
 
 namespace MVCForum.Website.Controllers
 {
@@ -21,6 +22,7 @@ namespace MVCForum.Website.Controllers
         // GET: Nyheder
         public ActionResult NewArticle()
         {
+            //ViewBag.ImageUploadType = "forumimageinsert";
             return View();
         }
         // GET: Nyheder
