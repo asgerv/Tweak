4ef023899bab1d1225c6469ad6680c83f4903918
asgerv <asgerv@gmail.com>
2016-06-24 17:17:16 +0200

    ArticleTag oprettelse og tilføjelse til artikel

--- MVCForum.Core/Interfaces/Services/IArticleTagService.cs
+++ MVCForum.Core/Interfaces/Services/IArticleTagService.cs
@@ -7,6 +7,7 @@ namespace MVCForum.Domain.Interfaces.Services
     public interface IArticleTagService
     {
         ArticleTag Add(ArticleTag articleTag);
+        void Add(string tags, Article article);
         void Delete(ArticleTag articleTag);
         ArticleTag Get(string tag);
         ArticleTag Get(Guid guid);
