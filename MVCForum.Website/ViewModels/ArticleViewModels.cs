53c083fb1d13033d4ed8ecd1fb2235166979a1cc
Asger <Asger@DESKTOP-88ACUPR>
2016-10-26 00:59:01 +0200

    #41 Mobil navigation fixet - Lavet dropdown afhængig af om man er i Nyheder, Video, Test eller Forum. - Opdateret pushy.css og pushy.js - Tilføjet arrow.svg til dropdown - Tilføjet mobil navigation ændringer i _CategoryNav - Tilføjet ArticleSection til ArticleCategoryNavViewModel - Ændret så forum (/Shared/SubNavigation) elementer bruger class pushy-link.

--- MVCForum.Website/ViewModels/ArticleViewModels.cs
+++ MVCForum.Website/ViewModels/ArticleViewModels.cs
@@ -15,14 +15,17 @@ namespace MVCForum.Website.ViewModels
         public string Header { get; set; }
         public string Description { get; set; }
         public string Body { get; set; }
+
         [UIHint("WrittenTimeAgo")]
         public DateTime PublishDate { get; set; }
+
         public string Image { get; set; }
         public MembershipUser User { get; set; }
         public string CategoryName { get; set; }
         public string CategorySlug { get; set; }
         public List<string> Tags { get; set; }
     }
+
     public class ArticleSearchViewModel
     {
         public string Slug { get; set; }
@@ -35,14 +38,18 @@ namespace MVCForum.Website.ViewModels
     {
         public string Slug { get; set; }
         public string Header { get; set; }
+
         [UIHint("WrittenTimeAgo")]
         public DateTime PublishDate { get; set; }
+
         public string UserName { get; set; }
         public string Image { get; set; }
     }
+
     public class CommentViewModel
     {
-        [UIHint(AppConstants.EditorType), AllowHtml]
+        [UIHint(AppConstants.EditorType)]
+        [AllowHtml]
         public string CommentBody { get; set; }
 
         public Guid? InReplyTo { get; set; }
@@ -50,6 +57,7 @@ namespace MVCForum.Website.ViewModels
         public Guid? CommentId { get; set; }
         public Guid? ArticleId { get; set; }
     }
+
     public class ArticleMainViewModel
     {
         public ArticleFrontpageViewModel Article1 { get; set; }
@@ -67,6 +75,7 @@ namespace MVCForum.Website.ViewModels
 
     public class ArticleCategoryNavViewModel
     {
+        public ArticleSection ArticleSection { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
         public int SortOrder { get; set; }
@@ -78,5 +87,4 @@ namespace MVCForum.Website.ViewModels
         public ArticleSection ArticleSection { get; set; }
         public ArticleSectionViewModel ArticleSectionViewModel { get; set; }
     }
-
 }
